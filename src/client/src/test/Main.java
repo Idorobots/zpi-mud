d8cfdfa4f8a1be9c70a40cb777f0f4ef9ac762a1
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2013-11-06 15:50:22 +0100

    Added Java client stub.

---
+++ src/client/src/test/Main.java
@@ -0,0 +1,65 @@
+package test;
+
+import io.socket.*;
+import org.json.*;
+
+class Main {
+    public static void main(String[] args) {
+        if(args.length < 3) {
+            System.out.println("Usage: client SERVER NICK PASSWORD");
+            return;
+        }
+
+        final String server = args[0];
+        final String nick = args[1];
+        final String password = args[2];
+
+        try {
+            System.out.println("Connecting to " + server + "...");
+            final SocketIO socket = new SocketIO(server);
+            socket.connect(new IOCallback() {
+                    @Override
+                    public void onMessage(String msg, IOAcknowledge ack) {
+                        // Ignored...
+                    }
+
+                    @Override
+                    public void onMessage(JSONObject msg, IOAcknowledge ack) {
+                        // Ignored...
+                    }
+
+                    @Override
+                    public void onError(SocketIOException socketIOException) {
+                        System.out.println("an Error occured");
+                        socketIOException.printStackTrace();
+                    }
+
+                    @Override
+                    public void onDisconnect() {
+                        System.out.println("Connection terminated.");
+                    }
+
+                    @Override
+                    public void onConnect() {
+                        System.out.println("Connection established!");
+
+                        try {
+                            socket.emit("authorize",
+                                        new JSONObject().put("nick", nick).put("password", password));
+                        } catch (JSONException jsonException) {
+                            System.out.println("an Error occured");
+                            jsonException.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void on(String event, IOAcknowledge ack, Object... args) {
+                        System.out.println("Received an event: '" + event + "'");
+                    }
+                });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
